e7e90920a1bc55dfe81cd1d55730985876807dd2
Tadesse Alemayehu <tadalem26@gmail.com>
2022-11-20 23:40:29 +0300

    Add: my-picture to the mobile display

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -2,16 +2,21 @@ import React from "react";
 import NavBar from "./partials/navBar";
 import { useState } from "react";
 import { Circle } from "./partials/decorations";
-import { Landing } from "./styles/LandingPage.styled";
+import { Landing, PrimaryImage } from "./styles/LandingPage.styled";
 import menu from "../icon/menu/menu.png";
+import my_picture from "../images/resized.svg";
 export default function LandingPage() {
-  let [showNavBar, updateDisplay] = useState(true);
+  let [showNavBar, updateDisplay] = useState(false);
   return (
     <Landing>
       <span className="showMenu" onClick={() => updateDisplay((old) => !old)}>
         <img src={menu} alt="open menus" />
       </span>
       <NavBar showNavBar={showNavBar} updateDisplay={updateDisplay} />
+      <PrimaryImage>
+        <img src={my_picture} alt="my-picture" />
+        <div className="blur-img"></div>
+      </PrimaryImage>
       <Circle
         moreStyle={{
           transform: "translate(-50%, -50%)",
--- src/components/styles/LandingPage.styled.js
+++ src/components/styles/LandingPage.styled.js
@@ -1,10 +1,35 @@
 import styled from "styled-components";
 
 export const Landing = styled.main`
-  padding: 10vw;
-  height: 80vh;
+  height: 100vh;
   .showMenu img {
+    padding: 5vw;
     width: 10vw;
-    float: right;
+    position: absolute;
+    right: 0;
+  }
+`;
+
+export const PrimaryImage = styled.div`
+  height: 50vh;
+  position: relative;
+  img {
+    height: 100%;
+    margin: auto;
+    display: block;
+  }
+  .blur-img {
+    width: 100%;
+    height: 100px;
+    background: rgb(7, 33, 68);
+    background: linear-gradient(
+      0deg,
+      rgba(7, 33, 68, 1) 0%,
+      rgba(7, 33, 68, 0.9332107843137255) 38%,
+      rgba(7, 33, 68, 0.6194852941176471) 69%,
+      rgba(7, 33, 68, 0.028448879551820738) 100%
+    );
+    position: absolute;
+    bottom: 0;
   }
 `;
