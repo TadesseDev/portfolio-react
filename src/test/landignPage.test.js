9493dfa56139bfed119f63687565b0f7b68ebcd8
Tadesse Alemayehu <tadalem26@gmail.com>
2022-11-22 14:40:20 +0300

    Update: customize test

--- src/test/landignPage.test.js
+++ src/test/landignPage.test.js
@@ -2,11 +2,11 @@ import { render, screen, fireEvent } from "@testing-library/react";
 // import userEvent from "@testing-library/user-event";
 // import "@testing-library/jest-dom";
 import App from "../App";
-
-test("Test menu bar", async () => {
-  // ARRANGE
+beforeEach(() => {
   render(<App />);
-  screen.getByText("Tadesse Dubale");
+});
+
+test("Test menu bar text content", async () => {
   screen.getByText("Home");
   screen.getByText("Skills");
   screen.getByText("Projects");
@@ -15,9 +15,19 @@ test("Test menu bar", async () => {
   screen.getByText("About");
   screen.getByText("Digital presence");
   screen.getByText("Contact me");
+});
+
+test("Test menu interaction", async () => {
   const navbar = await screen.findByTestId("navbar");
   expect(navbar.getAttribute("display")).toBe("none");
-  const toggleMenu = screen.getByAltText("open menus");
-  fireEvent.click(toggleMenu);
+  const showMenu = screen.getByAltText("open menus");
+  const hideMenu = screen.getByAltText("hide");
+  fireEvent.click(showMenu);
   expect(navbar.getAttribute("display")).toBe("block");
+  fireEvent.click(hideMenu);
+  expect(navbar.getAttribute("display")).toBe("none");
+});
+
+test("Test landing page text contents", async () => {
+  screen.getByText("Tadesse Dubale");
 });
