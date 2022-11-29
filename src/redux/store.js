d117aba4db0258baf92ce90e694f06a33b5d244e
Tadesse Alemayehu <tadalem26@gmail.com>
2022-11-29 16:46:16 +0300

    Configure: thunk and logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,9 @@
 import { createStoreHook } from "react-redux";
-import { combineReducers } from "redux";
+import { combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
 import skills from "./components/skills.js";
 const reducers = combineReducers({
   skills,
 });
-export default createStoreHook(reducers);
+export default createStoreHook(reducers, applyMiddleware(thunk, logger));
