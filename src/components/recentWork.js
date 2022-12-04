edf939be22e82e18ca594b457962a7d8544f01a2
Tadesse Alemayehu <tadalem26@gmail.com>
2022-12-04 16:16:13 +0300

    Update: recent work pre styles

--- src/components/recentWork.js
+++ src/components/recentWork.js
@@ -4,12 +4,16 @@ import meCoding from "../images/me-coding.svg";
 export default function recentWork() {
   return (
     <RecentWorkContainer>
-      <h2>recentWork</h2>
-      <p>I'm always learning, Working on personal or business software's </p>
-      <span className="image-container">
-        <img src={meCoding} alt="me coding on a table " />
-      </span>
-      <p>Here are some of the project I've been working on lately</p>
+      <div className="pre">
+        <h2>recentWork</h2>
+        <p>
+          I'm always Learning, Coding, Working on personal or business Projects
+        </p>
+        <span className="image-container">
+          <img src={meCoding} alt="me coding on a table " />
+        </span>
+        <p>Here are some of the project I've been working on lately</p>
+      </div>
     </RecentWorkContainer>
   );
 }
--- src/components/styles/recentWork.styed.js
+++ src/components/styles/recentWork.styed.js
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 
 export const RecentWorkContainer = styled.div`
+  p {
+    padding: 5vw;
+  }
   margin: 10vh 0;
   text-align: center;
   img {
