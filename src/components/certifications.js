ff6690c60dfe67fdeeca7be677f4fb42d8587e88
Tadesse-Alemayehu <tadalem26@gmail.com>
2023-01-05 17:19:57 +0300

    Update certification description

--- src/components/certifications.js
+++ src/components/certifications.js
@@ -5,6 +5,7 @@ import {
   Certification,
 } from "./styles/certifications.styled";
 import PrimaryTitle from "./partials/primaryTitle";
+import SubTitle from "./partials/subTitle";
 import { getCertifications } from "../redux/components/certifications";
 export default function Certifications() {
   const dispatch = useDispatch();
@@ -13,9 +14,16 @@ export default function Certifications() {
   return (
     <CertificationsContainer>
       <PrimaryTitle icon="" text={"Certifications"} />
-      {certifications.map((certification) => (
-        <Certification>{certification.title}</Certification>
-      ))}
+      {certifications.map(
+        ({ id, title = "", description = "", link = "", img = "" }) => (
+          <Certification key={id}>
+            <SubTitle text={title} />
+            <div>
+              <p>{ description}</p>
+            </div>
+          </Certification>
+        )
+      )}
     </CertificationsContainer>
   );
 }
