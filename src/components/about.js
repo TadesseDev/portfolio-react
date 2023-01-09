0b6f088a7e7a35c0bdcc91ecd3e1a38384cb2f65
Tadesse-Alemayehu <tadalem26@gmail.com>
2023-01-09 17:12:43 +0300

    Update an about div to AboutContainer styled component

--- src/components/about.js
+++ src/components/about.js
@@ -1,24 +1,39 @@
 import React from "react";
 import PrimaryTitle from "./partials/primaryTitle";
-
+import SubTitle from "./partials/subTitle";
+import AboutContainer from "./styles/about.styled";
 export default function about() {
   return (
-    <div style={{ textAlign: "center" }}>
+    <AboutContainer>
       <PrimaryTitle text="About" />
-      <p style={{ color: "white" }}>
-        Algorithm and Data Structure enthusiast. What impress me the most is to
-        deal with the time and memory efficiency. If you like to impress me
-        optimize my code to a point it perform 1 microsecond better.
+      <p>
+        learning a language is not the the point, Understand the fundamental of
+        <span className="bright"> CS </span>
+        then you can master what ever language or framework there is to it.
+        <br />
+        <span className="bright">
+          "it is a matter of reading and writhing a new syntax"
+        </span>
+        .
       </p>
-      <p style={{ color: "white" }}>
+      <p>
+        Algorithm and Data Structure enthusiast. What interestl me the most is
+        to deal with the <span className="bright">time</span> and{" "}
+        <span className="bright">memory</span> efficiency. If you like to
+        impress me optimize my code to a point it perform 1 microsecond better.
+        <br />
+        <span className="emoji">🙌</span>
+      </p>
+      <p>
         I love writhing clean code, As they say “
-        <span style={{ color: "var(--bright)" }}>
+        <span className="bright">
           Any fool can write code that a computer can understand. Good
           programmers write code that humans can understand.
         </span>
-        ” 😊
+        ” .<br />
+        <span className="emoji">😊</span>
       </p>
-      
-    </div>
+      <SubTitle text="You can find me..."/>
+    </AboutContainer>
   );
 }
---
+++ src/components/styles/about.styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+const about = styled.div`
+  text-align: center;
+  p {
+    color: white;
+  }
+  .bright {
+    color: var(--bright);
+  }
+  .emoji {
+    font-size: 2rem;
+  }
+`;
+export default about;
