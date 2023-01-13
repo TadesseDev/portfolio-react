8831b0bcd1487e0285e35ba120cb10dda710f183
Tadesse-Alemayehu <tadalem26@gmail.com>
2023-01-13 21:15:39 +0300

    render basic from to the contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,5 +1,34 @@
 import React from "react";
-
+import PrimaryTitle from "./partials/primaryTitle";
+import user_contact from "../icon/user_contact.svg"
+import email from "../icon/email.svg"
+import phone from "../icon/phone.svg";
+import SubmitButton from "./partials/flyButton"
+import ContactContainer, {
+  InputField,
+  TextField,
+} from "./styles/contact.styled";
 export default function contact() {
-  return <div>contact</div>;
+  return (
+    <ContactContainer>
+      <PrimaryTitle text="Contact me" />
+      <form action="">
+        <InputField icon={user_contact}>
+          <input type="text" name="user-name" id="user-name" />
+        </InputField>
+        <InputField icon={email}>
+          <input type="email" name="user-email" id="user-email" />
+        </InputField>
+        <InputField icon={phone}>
+          <input type="number" name="phone-number" id="phone-number" />
+        </InputField>
+        <TextField>
+          <textarea name="message" id="message" cols="30" rows="10"></textarea>
+        </TextField>
+        <button type="submit">
+          <SubmitButton text="Send" />
+        </button>
+      </form>
+    </ContactContainer>
+  );
 }
---
+++ src/components/styles/contact.styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const ContactContainer = styled.div``;
+export const InputField = styled.div``;
+export const TextField = styled.div``;
+
+export default ContactContainer;
