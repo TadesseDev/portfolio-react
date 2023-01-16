691aad53e4f9fd1f2ab829ee93280f29d9027a13
Tadesse Alemayehu <tadalem26@gmail.com>
2023-01-16 12:26:06 +0300

    Fix contact form validation

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,44 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import PrimaryTitle from "./partials/primaryTitle";
-import user_contact from "../icon/user_contact.svg"
-import email from "../icon/email.svg"
+import user_contact from "../icon/user_contact.svg";
+import email from "../icon/email.svg";
 import phone from "../icon/phone.svg";
-import SubmitButton from "./partials/flyButton"
+import SubmitButton from "./partials/flyButton";
 import ContactContainer, {
   InputField,
   TextField,
 } from "./styles/contact.styled";
-export default function contact() {
+export default function Contact() {
+  const [formData, updateData] = useState({
+    full_name: "",
+    email: "",
+    phone_number: "",
+    message: "",
+  });
+  const updateFormData = (e) => {
+    const val = e.target.value;
+    const name = e.target.name;
+    updateData((old) => ({ ...old, [name]: val }));
+  };
+  const submitMessage = e => {
+    e.preventDefault();
+    console.log(e)
+    console.log(JSON.stringify(formData));
+  }
   return (
     <ContactContainer>
-      <PrimaryTitle text="Contact me" style={{color: "var(--primary)"}} />
-      <form action="">
-        <InputField icon={user_contact}>
+      <PrimaryTitle text="Contact me" style={{ color: "var(--primary)" }} />
+      <form action="" onSubmit={submitMessage}>
+        <InputField>
           <img className="icon" src={user_contact} alt="User Name" />
           <input
             type="text"
-            name="user-name"
+            name="full_name"
             id="user-name"
             placeholder="Full Name..."
+            required={true}
+            value={formData["full_name"]}
+            onChange={updateFormData}
           />
         </InputField>
-        <InputField icon={email}>
+        <InputField>
           <img className="icon" src={email} alt="User Email" />
-          <span icon></span>
+
           <input
             type="email"
-            name="user-email"
+            name="email"
             id="user-email"
             placeholder="Email..."
+            required={true}
+            value={formData["email"]}
+            onChange={updateFormData}
           />
         </InputField>
-        <InputField icon={phone}>
+        <InputField>
           <img className="icon" src={phone} alt="Phone number" />
           <input
             type="number"
-            name="phone-number"
+            name="phone_number"
             id="phone-number"
             placeholder="Phone number..."
+            value={formData["phone_number"]}
+            onChange={updateFormData}
           />
         </InputField>
         <TextField>
@@ -47,10 +71,19 @@ export default function contact() {
             id="message"
             rows="10"
             placeholder="Your thought..."
+            required={true}
+            value={formData["message"]}
+            onChange={updateFormData}
           ></textarea>
         </TextField>
-        <button type="submit">
-          <SubmitButton text="Send" />
+
+        <button
+          type="submit"
+          onClick={(e) => {
+            e.target.parentElement.click();
+          }}
+        >
+          <SubmitButton text="Send" preventDefault={true} />
         </button>
       </form>
     </ContactContainer>
