357dd0f651900044a734f7f832f72ea0eef800bb
Tadesse-Alemayehu <tadalem26@gmail.com>
2023-01-23 00:26:22 +0300

    Lazy load testimonials

--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { LazyLoadComponent } from "react-lazy-load-image-component";
 import { useDispatch, useSelector } from "react-redux";
 import { getTestimonials } from "../redux/components/testimonials";
 import PrimaryTitle from "./partials/primaryTitle";
@@ -27,70 +28,79 @@ export default function Testimonials() {
           ></SubTitle>
           <SubTitle
             text={testimony.title}
-            style={{ fontSize: "0.7rem", color: "var(--bright)", margin: "0" }}
-          ></SubTitle>
-          <TestimonyCard
-            testimonyImage={testimony.image}
-            backgroundImage={
-              testimony.id % 2 === 0 ? testimonialBlob_flip : testimonialBlob
-            }
-          >
-            <span className="testimonial-img"></span>
-            <a href={"mailto:" + testimony.email}>{testimony.email}</a>
-
-            <p>
-              <span className="text-content">
-                {testimony.testimony.slice(0, 200)}
-              </span>
-              <button
-                type="submit"
-                className="link-button more"
-                onClick={(e) => {
-                  const textContainer =
-                    e.target.parentNode.getElementsByClassName(
-                      "text-content"
-                    )[0];
-                  const more =
-                    e.target.parentNode.getElementsByClassName("more")[0];
-                  const less =
-                    e.target.parentNode.getElementsByClassName("less")[0];
-                  textContainer.textContent = testimony.testimony;
-                  more.style.display = "none";
-                  less.style.display = "inline";
-                }}
-              >
-                ...more
-              </button>
-              <button
-                type="submit"
-                className="link-button less"
-                style={{ display: "none"}}
-                onClick={(e) => {
-                  const textContainer =
-                    e.target.parentNode.getElementsByClassName(
-                      "text-content"
-                    )[0];
-                  const more =
-                    e.target.parentNode.getElementsByClassName("more")[0];
-                  const less =
-                    e.target.parentNode.getElementsByClassName("less")[0];
-                  textContainer.textContent = testimony.testimony.slice(0, 200);
-                  more.style.display = "inline";
-                  less.style.display = "none";
-                }}
-              >
-                ...less{" "}
-              </button>
-            </p>
-          </TestimonyCard>
-          <SocialMediaIcons
-            links={{
-              linkedIn: { link: testimony.linkedin },
-              twitter: { link: testimony.twitter },
-              gitHub: { link: testimony.github },
+            style={{
+              fontSize: "0.7rem",
+              color: "var(--bright)",
+              margin: "0",
             }}
-          />
-          <hr />
+          ></SubTitle>
+          <LazyLoadComponent>
+            <TestimonyCard
+              testimonyImage={testimony.image}
+              backgroundImage={
+                testimony.id % 2 === 0 ? testimonialBlob_flip : testimonialBlob
+              }
+            >
+              <span className="testimonial-img"></span>
+              <a href={"mailto:" + testimony.email}>{testimony.email}</a>
+
+              <p>
+                <span className="text-content">
+                  {testimony.testimony.slice(0, 200)}
+                </span>
+                <button
+                  type="submit"
+                  className="link-button more"
+                  onClick={(e) => {
+                    const textContainer =
+                      e.target.parentNode.getElementsByClassName(
+                        "text-content"
+                      )[0];
+                    const more =
+                      e.target.parentNode.getElementsByClassName("more")[0];
+                    const less =
+                      e.target.parentNode.getElementsByClassName("less")[0];
+                    textContainer.textContent = testimony.testimony;
+                    more.style.display = "none";
+                    less.style.display = "inline";
+                  }}
+                >
+                  ...more
+                </button>
+                <button
+                  type="submit"
+                  className="link-button less"
+                  style={{ display: "none" }}
+                  onClick={(e) => {
+                    const textContainer =
+                      e.target.parentNode.getElementsByClassName(
+                        "text-content"
+                      )[0];
+                    const more =
+                      e.target.parentNode.getElementsByClassName("more")[0];
+                    const less =
+                      e.target.parentNode.getElementsByClassName("less")[0];
+                    textContainer.textContent = testimony.testimony.slice(
+                      0,
+                      200
+                    );
+                    more.style.display = "inline";
+                    less.style.display = "none";
+                  }}
+                >
+                  ...less{" "}
+                </button>
+              </p>
+            </TestimonyCard>
+            <SocialMediaIcons
+              links={{
+                linkedIn: { link: testimony.linkedin },
+                twitter: { link: testimony.twitter },
+                gitHub: { link: testimony.github },
+              }}
+            />
+            <hr />
+          </LazyLoadComponent>
         </Testimonial>
       ))}
     </TestimonialContainer>
