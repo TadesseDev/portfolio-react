78f14a6d9a32b774aebdef010a052d5a7f02de39
TadesseDev <tadalem26@gmail.com>
2023-06-05 17:55:17 +0000

    Jun 5, 2023, 8:55 PM

--- src/test/landignPage.test.js
+++ src/test/landignPage.test.js
@@ -12,7 +12,7 @@ beforeAll(async () => {
 describe("Navbar items test", () => {
   test("Menu bar text content", async () => {
     render(<NavBar />);
-    getByText("Home");
+    screen.getByText("Home");
     screen.getByText("Skills");
     screen.getByText("Projects");
     screen.getByText("Certificates");
@@ -68,4 +68,7 @@ describe("Navbar items test", () => {
   });
 });
 
-describe("test homepage items", () => {});
+
+describe("test homepage items", () => {
+
+})
