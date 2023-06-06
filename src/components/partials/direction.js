c5fa15210e89608d1ee793b197b247fa131a6ea6
TadesseDev <tadalem26@gmail.com>
2023-06-06 20:03:01 +0000

    Jun 6, 2023, 11:03 PM

--- src/components/partials/direction.js
+++ src/components/partials/direction.js
@@ -3,7 +3,13 @@ import more from "../../icon/directions/more.png";
 import show from "../../icon/directions/show.png";
 import less from "../../icon/directions/less.png";
 import DirectionContainer from "./styles/direction.styled";
-const Direction = ({ icon, toggle = () => {}, style = {}, className = "", text="" }) => {
+const Direction = ({
+  icon,
+  toggle = () => {},
+  style = {},
+  className = "",
+  text = "",
+}) => {
   let iconMap = new Map([
     ["hide", hide],
     ["show", show],
@@ -11,17 +17,14 @@ const Direction = ({ icon, toggle = () => {}, style = {}, className = "", text="
     ["less", less],
   ]);
   return (
-    <>
-      <DirectionContainer
-        className={"direction " + className}
-        onClick={toggle}
-        style={style}
-        text="Show Menu"
-      >
-        <img src={iconMap.get(icon)} alt={icon} width="15px" height="15px" />
-      </DirectionContainer>
-      {text && (<p>{text}</p>)}
-    </>
+    <DirectionContainer
+      className={"direction " + className}
+      onClick={toggle}
+      style={style}
+      text="Show Menu"
+    >
+      <img src={iconMap.get(icon)} alt={icon} width="15px" height="15px" />
+    </DirectionContainer>
   );
 };
 
