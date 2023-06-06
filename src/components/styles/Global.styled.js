164b3a158ae1507fc24f5db77434667edf0a784f
TadesseDev <tadalem26@gmail.com>
2023-06-06 12:17:55 +0000

    Update scrollbar and menu ites for desktop display

--- src/components/styles/Global.styled.js
+++ src/components/styles/Global.styled.js
@@ -56,9 +56,7 @@ background: linear-gradient(180deg, rgba(233,238,96,1) 0%, rgba(59,175,192,1) 10
     margin: 0;
     padding: 0;
     display: flex;
-   }
-   html{
-          ::-webkit-scrollbar {
+      ::-webkit-scrollbar {
     width: 10px;
   }
 
